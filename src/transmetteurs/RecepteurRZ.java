73f35d0b380e4267d69744b11e275a99f8e0a821
Arouna <kane.arouna94@gmail.com>
2016-10-16 16:51:31 +0200

    RecepteurRZ
    First part

--- src/transmetteurs/RecepteurRZ.java
+++ src/transmetteurs/RecepteurRZ.java
@@ -1,20 +1,31 @@
 package transmetteurs;
 
+import destinations.DestinationInterface;
 import information.Information;
 import information.InformationNonConforme;
 
 public class RecepteurRZ extends Transcepteur<Float, Boolean> {
+	
+	private final float seuilDeDetection = (max - min)/2;
 
 	@Override
 	public void recevoir(Information<Float> information) throws InformationNonConforme {
-		// TODO Auto-generated method stub
-		
+		informationRecue = information;
+		emettre();
 	}
 
 	@Override
 	public void emettre() throws InformationNonConforme {
-		// TODO Auto-generated method stub
-		
+		Information<Boolean> signalRZ = this.decodageRZ();
+		for (DestinationInterface<Boolean> destinationConnectee : destinationsConnectees) {
+			destinationConnectee.recevoir(signalRZ);
+		}
+		this.informationEmise = signalRZ;
 	}
-
+	
+	private Information<Boolean> decodageRZ() {
+		Information<Boolean> signalRZ = new Information<Boolean>();
+		
+		return signalRZ;
+	}
 }
