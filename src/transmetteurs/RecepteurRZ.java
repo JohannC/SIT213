d7810cfa6018886b176a1898c0d391fa0d5635c9
Arouna <kane.arouna94@gmail.com>
2016-10-16 15:49:29 +0200

    Recepteurs Classes Created

---
+++ src/transmetteurs/RecepteurRZ.java
@@ -0,0 +1,20 @@
+package transmetteurs;
+
+import information.Information;
+import information.InformationNonConforme;
+
+public class RecepteurRZ extends Transcepteur<Float, Boolean> {
+
+	@Override
+	public void recevoir(Information<Float> information) throws InformationNonConforme {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void emettre() throws InformationNonConforme {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
