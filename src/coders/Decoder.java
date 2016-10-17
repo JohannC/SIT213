eced661aed20ff6cdf323f320aa87e9f7e63bfef
lkchao78 <limkevin.chao@gmail.com>
2016-10-17 20:55:17 +0200

    javadoc Decoder v2

--- src/coders/Decoder.java
+++ src/coders/Decoder.java
@@ -2,7 +2,12 @@ package coders;
 
 /**
  * La classe Decoder permet de décoder l'information en réception
- *
+ * Cette classe implémente l'interface IDecoder.
+ * Il s'agit d'une classe abstraite qui s'occupe de factoriser les attributs suivants :
+ * - nbEchantillon : nombre d'échantillons par bit
+ * - min : amplitude minimale du signal analogique
+ * - max : amplitude maximale du signal analogique
+ * - seuilDeDetection : seuil permettant de décider entre un bit '0' et un bit '1'
  */
 
 public abstract class Decoder implements IDecoder {
@@ -13,7 +18,9 @@ public abstract class Decoder implements IDecoder {
 	protected float seuilDeDetection;
 
 	/**
-	 * Construteur de la classe Decoder
+	 * Constructeur de la classe Decoder.
+	 * S'occupe d'attribuer les valeurs passées en arguments aux attributs de la classe.
+	 * Calcule le seuil de détection à partir des valeurs extrêmes du signal analogique.
 	 * 
 	 * @param min
 	 *            amplitude minimale
