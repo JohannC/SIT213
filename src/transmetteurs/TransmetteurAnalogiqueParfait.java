076ef90ae004a97408bbacd5c4fced6b9103c62a
Arouna <kane.arouna94@gmail.com>
2016-10-17 20:33:23 +0200

    javadoc done

--- src/transmetteurs/TransmetteurAnalogiqueParfait.java
+++ src/transmetteurs/TransmetteurAnalogiqueParfait.java
@@ -3,15 +3,24 @@ package transmetteurs;
 import destinations.DestinationInterface;
 import information.Information;
 import information.InformationNonConforme;
-
+/**
+ * Un transmetteur analogique parfait reçoit une information<Float> et le retransmet telle qu'elle
+ *
+ */
 public class TransmetteurAnalogiqueParfait extends Transmetteur<Float, Float> {
 
+	/**
+	 * Méthode pour reçevoir une information (un signal)
+	 */
 	@Override
 	public void recevoir(Information<Float> information) throws InformationNonConforme {
 		informationRecue = information;	
 		emettre();
 	}
 
+	/**
+	 * Méthode pour emettre une information (un signal)
+	 */
 	@Override
 	public void emettre() throws InformationNonConforme {
 		for (DestinationInterface <Float> destinationConnectee : destinationsConnectees) {
