a3b94c71066d05cf5a9d7cb2372a7375de4b78ad
Arouna <kane.arouna94@gmail.com>
2016-10-17 19:16:33 +0200

    Decodeur fixed

--- src/coders/Decoder.java
+++ src/coders/Decoder.java
@@ -1,17 +1,32 @@
 package coders;
 
+/**
+ * La classe Decoder permet de décoder l'information en réception
+ *
+ */
+
 public abstract class Decoder implements IDecoder {
-	
+
 	protected int nbEchantillon;
 	protected float min;
 	protected float max;
 	protected float seuilDeDetection;
-	
-	public Decoder(float min, float max, int nbEchantillon){
+
+	/**
+	 * Construteur de la classe Decoder
+	 * 
+	 * @param min
+	 *            amplitude minimale
+	 * @param max
+	 *            amplitude maximale
+	 * @param nbEchantillon
+	 *            nombre d'échantillons par bit
+	 */
+	public Decoder(float min, float max, int nbEchantillon) {
 		this.nbEchantillon = nbEchantillon;
 		this.min = min;
 		this.max = max;
-		seuilDeDetection = (max - min) / 2;
+		seuilDeDetection = (max + min) / 2;
 	}
-	
+
 }
--- src/coders/DecoderNRZ.java
+++ src/coders/DecoderNRZ.java
@@ -2,24 +2,42 @@ package coders;
 
 import information.Information;
 
+/**
+ * DecoderNRZ permet de décoder un signal NRZ
+ * 
+ * @author Arouna
+ *
+ */
 public class DecoderNRZ extends Decoder {
-
+	/**
+	 * onstructeur pour initialiser le décodeur NRZ
+	 * @param min
+	 *            amplitude minimale
+	 * @param max
+	 *            amplitude maximale
+	 * @param nbEchantillon
+	 *            nombre d'échantillons par bit
+	 */
 	public DecoderNRZ(float min, float max, int nbEchantillon) {
 		super(min, max, nbEchantillon);
 	}
 
+	/**
+	 * Méthode pour décoder le signal NRZ
+	 * Elle transforme un Information<Float> en Information<Boolean>
+	 */
 	@Override
 	public Information<Boolean> decode(Information<Float> msg) {
-		Information<Boolean> informationNRZ = new Information<Boolean>(); 
-		
-		for (int i = 0; i < (msg.nbElements()/nbEchantillon); i++) {
-			if (msg.iemeElement(i*nbEchantillon) > seuilDeDetection) {
-					informationNRZ.add(true);
+		Information<Boolean> informationNRZ = new Information<Boolean>();
+
+		for (int i = 0; i < (msg.nbElements() / nbEchantillon); i++) {
+			if (msg.iemeElement(i * nbEchantillon) > seuilDeDetection) {
+				informationNRZ.add(true);
 			} else {
-					informationNRZ.add(false);
+				informationNRZ.add(false);
 			}
 		}
-		
+
 		return informationNRZ;
 	}
 
