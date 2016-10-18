4aae8ffb4740c56a5fa4e9230760aa9dcfc363f0
lkchao78 <limkevin.chao@gmail.com>
2016-10-18 16:37:48 +0200

    Ajout méthode calculPuissanceBruit
    Ajout méthode calculPuissanceBruit dans la classe TransmetteurBruite

--- src/transmetteurs/TransmetteurBruite.java
+++ src/transmetteurs/TransmetteurBruite.java
@@ -8,7 +8,8 @@ public class TransmetteurBruite extends Transmetteur<Float, Float> {
 	@Override
 	public void recevoir(Information<Float> information) throws InformationNonConforme {
 		// TODO Auto-generated method stub
-		
+		informationRecue = information;
+		emettre();
 	}
 
 	@Override
@@ -17,4 +18,13 @@ public class TransmetteurBruite extends Transmetteur<Float, Float> {
 		
 	}
 
+	private float calculPuissanceBruit(Information<Float> informationRecue, float snr){
+		float pBruit;
+		float sommeCarreSignal = 0f;
+		for (float info : informationRecue)
+			sommeCarreSignal += Math.pow(info,2); 
+		float pSignal= sommeCarreSignal / informationRecue.nbElements();
+		pBruit = (float) (pSignal/Math.pow(10,(snr/10)));
+		return pBruit;
+	}
 }
