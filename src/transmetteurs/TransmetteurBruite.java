58edb033c3d19f7c13fe9f401257aeddaf9ecac4
lkchao78 <limkevin.chao@gmail.com>
2016-10-18 16:23:50 +0200

    Création de la classe TransmetteurBruite

---
+++ src/transmetteurs/TransmetteurBruite.java
@@ -0,0 +1,20 @@
+package transmetteurs;
+
+import information.Information;
+import information.InformationNonConforme;
+
+public class TransmetteurBruite extends Transmetteur<Float, Float> {
+
+	@Override
+	public void recevoir(Information<Float> information) throws InformationNonConforme {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void emettre() throws InformationNonConforme {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
