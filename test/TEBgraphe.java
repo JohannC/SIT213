1e58a755d967e3e5f5461c553358121698e6eb56
lkchao78 <limkevin.chao@gmail.com>
2016-11-01 16:33:07 +0100

    classe TEBgraphe.java + fichier csv
    Classe pour générer le graphe

---
+++ test/TEBgraphe.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.FileWriter;
+
+import exception.ArgumentsException;
+
+public class TEBgraphe {
+
+	private static final int nbEch = 30;
+
+	public static void main(String[] args) throws ArgumentsException {
+		File file = new File("simuTEB.csv");
+		FileWriter fw;
+		try {
+			file.createNewFile();
+			fw = new FileWriter(file);
+
+			for (float snr = -30f; snr <= 0f; snr = snr + 0.1f) {
+				String[] args1 = { "-form", "NRZT", "-snr", "" + Math.round(snr * 100.0) / 100.0 };
+				Simulateur simu = new Simulateur(args1);
+				float teb;
+				simu.execute();
+				teb = simu.calculTauxErreurBinaire();
+				float snr1 = snr + (float) (10*Math.log10(nbEch/2)); 
+				fw.write(""+snr1+";"+teb+"\n");
+
+			}
+			fw.flush();
+			fw.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
