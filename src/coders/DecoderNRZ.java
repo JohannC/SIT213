06a586ebd8927162295e76474874b5372a1483fb
CORCUFF Johann <JRI1@dsone.3ds.com>
2016-11-01 15:34:25 +0100

    Decodeur NRZ amélioré

--- src/coders/DecoderNRZ.java
+++ src/coders/DecoderNRZ.java
@@ -31,13 +31,19 @@ public class DecoderNRZ extends Decoder {
 	public Information<Boolean> decode(Information<Float> msg) {
 		Information<Boolean> informationNRZ = new Information<Boolean>();
 		float seuil = calculSeuil(msg);
-		for (int i = 0; i < (msg.nbElements() / nbEchantillon); i++) {
-			
-			if (msg.iemeElement(i * nbEchantillon) > seuil) {
-				informationNRZ.add(true);
-			} else {
-				informationNRZ.add(false);
+		for (int i = 0; i < msg.nbElements(); i += nbEchantillon) {
+			float moyenne = 0.0f;
+			float somme = 0.0f;
+			int t1 = nbEchantillon / 3;
+			int t3 = t1;
+			int t2 = nbEchantillon - t1 - t3;
+
+			for (int j = 0; j < t2; j++) {
+				somme += msg.iemeElement(i + t1 + j);
 			}
+
+			moyenne = somme / t2;
+			informationNRZ.add(moyenne > seuil);
 		}
 
 		return informationNRZ;
