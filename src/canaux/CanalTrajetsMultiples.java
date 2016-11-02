d9c74a717299bc6b5585cf5dda9bbf21d0111785
lkchao78 <limkevin.chao@gmail.com>
2016-11-02 15:05:26 +0100

    CanalTrajetsMultiples v0.1
    By CHAO Lim Kévin & AMHOUCHE Lahoucine

---
+++ src/canaux/CanalTrajetsMultiples.java
@@ -0,0 +1,31 @@
+package canaux;
+
+import information.Information;
+
+public class CanalTrajetsMultiples {
+	
+	private Information <Float> informationRecue;
+	private Information <Float> informationRetardee = null;
+	private int retard;
+	private float attenuation;
+	
+	public void CanalTrajetsMultiple(Information<Float> information, int retard, float attenuation){
+		informationRecue = information;
+		this.retard = retard;
+		this.attenuation = attenuation;
+	}
+
+	public Information<Float> monInfoRetardee(){
+		informationRetardee = new Information<Float>();
+		for (int i = 0 ; i < retard ; i++)
+			informationRetardee.add(0f);
+		
+		for (float e : informationRecue)
+			informationRetardee.add(e*attenuation);
+		
+		return informationRetardee;
+	}
+
+
+
+}
---
+++ test/canaux/CanalTrajetsMultiplesTest.java
@@ -0,0 +1,25 @@
+package canaux;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import information.Information;
+
+public class CanalTrajetsMultiplesTest {
+	
+	Information<Float> informationRecue;
+	
+	@Before
+	public void setUp() throws Exception {
+		informationRecue = new Information<Float>();
+		for (int i=0 ; i < 180 ;  )
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
