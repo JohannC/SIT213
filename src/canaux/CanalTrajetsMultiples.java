a0574702f33e58daa416112616d0bb26dea0a09c
lkchao78 <limkevin.chao@gmail.com>
2016-11-06 16:57:42 +0100

    Javadoc 1.0 CanalTrajetsMultiples
    Javadoc by LKC

--- src/canaux/CanalTrajetsMultiples.java
+++ src/canaux/CanalTrajetsMultiples.java
@@ -2,6 +2,10 @@ package canaux;
 
 import information.Information;
 
+/**
+ * Classe CanalTrajetsMultiples
+ * A partir du signal recu va generer un signal retarde et attenue
+ */
 public class CanalTrajetsMultiples {
 	
 	private Information <Float> informationRecue;
@@ -9,12 +13,23 @@ public class CanalTrajetsMultiples {
 	private int retard;
 	private float attenuation;
 	
+	/**
+	 * Construction de la classe CanalTrajetsMultiples
+	 * S'occupe d'affecter à ses attributs les parametres associes
+	 * @param information le signal recu de type Information<Float>
+	 * @param retard 
+	 * @param attenuation
+	 */
 	public void CanalTrajetsMultiple(Information<Float> information, int retard, float attenuation){
 		informationRecue = information;
 		this.retard = retard;
 		this.attenuation = attenuation;
 	}
 
+	/**
+	 * Methode qui s'occupe de creer le signal retarde et attenue
+	 * 
+	 */
 	public Information<Float> monInfoRetardee(){
 		informationRetardee = new Information<Float>();
 		for (int i = 0 ; i < retard ; i++)
