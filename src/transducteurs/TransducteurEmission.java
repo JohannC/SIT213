2a81c4136be6ab04c100e8ae4edc9b93a5a98de7
lkchao78 <limkevin.chao@gmail.com>
2016-11-08 21:24:03 +0100

    Javadoc 1.0 TransducteurEmission
    Javadoc by LKC

--- src/transducteurs/TransducteurEmission.java
+++ src/transducteurs/TransducteurEmission.java
@@ -5,19 +5,27 @@ import information.Information;
 import information.InformationNonConforme;
 import transmetteurs.Transmetteur;
 
+/**
+ * Classe TransducteurEmission qui herite de la classe abstraite Transmetteur.
+ * A pour but de coder la source du signal.
+ */
 public class TransducteurEmission extends Transmetteur<Boolean, Boolean> {
-
-	public TransducteurEmission(){
-		super();
-	}
 	
+	/**
+	 * Methode publique pour recevoir une information. 
+	 * @param information 
+	 * 		Le message a encoder de type Information<Boolean>.
+	 */
 	@Override
 	public void recevoir(Information<Boolean> information) throws InformationNonConforme {
 		informationRecue = information;
 		codageDeSource();
 		emettre();
 	}
-
+	
+	/**
+	 * Methode publique pour emettre une information. 
+	 */
 	@Override
 	public void emettre() throws InformationNonConforme {
 		for (DestinationInterface<Boolean> destinationConnectee : destinationsConnectees) {
@@ -25,6 +33,9 @@ public class TransducteurEmission extends Transmetteur<Boolean, Boolean> {
 		}
 	}
 	
+	/**
+	 * Methode privee pour encoder un message.
+	 */
 	private void codageDeSource () {
 		
 		Information<Boolean> info = new Information<Boolean>();
